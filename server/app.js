90e3a84a68098a092e9deab1b202e33973fd305f
nagyrobert92 <nagyrobert1992@yahoo.com>
2019-07-12 01:35:42 +0200

    schema definition

--- server/app.js
+++ server/app.js
@@ -3,6 +3,8 @@ const graphqlHTTP = require("express-graphql");
 
 const app = express();
 
+app.use("graphql", graphqlHTTP({}));
+
 app.listen(4000, () => {
   console.log("Listening for requests on port 4000");
 });
